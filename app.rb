f62f9756328d5e9768cae2401325e76c6de4ad5b
mesmerze <aichernovmail@gmail.com>
2017-07-11 16:06:28 +0300

    Vk authentication testing

--- app.rb
+++ app.rb
@@ -4,8 +4,27 @@ require './config/environments'
 require './models/model'
 require 'yaml'
 require 'ostruct'
+# require 'URI'
+require 'net/http'
 
 get('/') do
+  # uri = URI('https://oauth.vk.com/authorize')
+  # params = {client_id: 6109521,
+  #           display: 'page',
+  #           redirect_uri: '/index',
+  #           response_type: 'token'}
+  # uri.query = URI.encode_www_form(params)
+  # http = Net::HTTP.new(uri.host, uri.port)
+  # http.use_ssl = true
+  # http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+  # request = Net::HTTP::Get.new(uri.request_uri)
+  # response = http.request(request)
+  # @data = response.body
+
+  erb :hello
+end
+
+get('/index') do
   erb :index
 end
 
