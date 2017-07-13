4a1c194a48c3c27867f5ee66856f7f6a13795c38
mesmerze <aichernovmail@gmail.com>
2017-07-13 20:36:19 +0300

    Refactore login process

--- app.rb
+++ app.rb
@@ -8,52 +8,53 @@ require 'net/http'
 require 'vk'
 require './jwt.rb'
 get('/') do
-  erb :hello
-end
-
-get('/login') do
-  code = params[:code] if params[:code]
-  uri = URI('https://oauth.vk.com/access_token')
-  shmarams = { client_id: 6109521,
-               client_secret: 'MsuqFHPDMEY0jpE2Jeyy',
-               redirect_uri: 'https://shawarmas.herokuapp.com/login',
-               code: code
-  }
-  uri.query = URI.encode_www_form(shmarams)
-  http = Net::HTTP.get_response(uri)
-  my_hash = JSON.parse(http.body)
-  @users = User.all
-  headers = {
-      exp: Time.now.to_i + 60 #expire in 360 seconds
-    }
-
-  unless @users.where(user_id: my_hash['user_id'])
-    @user = User.new(my_hash)
-    @user.save
-    @token = JWT.encode({user_id: my_hash['user_id']},
-                         settings.signing_key,
-                         "RS256",
-                         headers)
-    session['access_token'] = @token
-  else
-    @token = JWT.encode({user_id: my_hash['user_id']},
-                         settings.signing_key,
-                         "RS256",
-                         headers)
-    session['access_token'] = @token
-  end
-  redirect('/index')
-
-  erb :login
-end
-
-get('/index') do
   protected!
   Vk.app_id = 6109521
   Vk.app_secret = 'MsuqFHPDMEY0jpE2Jeyy'
   vk = Vk.client
   @profile = vk.request 'users.get', user_ids: @user_id
   puts @profile.each { |user| user['first_name'] }.first['first_name']
+  erb :index
+end
+
+get('/login') do
+  if params[:code]
+    code = params[:code]
+    uri = URI('https://oauth.vk.com/access_token')
+    shmarams = { client_id: ENV['APP_ID'],
+                 client_secret: 'MsuqFHPDMEY0jpE2Jeyy',
+                 redirect_uri: 'https://shawarmas.herokuapp.com/login',
+                 code: code
+    }
+    uri.query = URI.encode_www_form(shmarams)
+    http = Net::HTTP.get_response(uri)
+    my_hash = JSON.parse(http.body)
+    @users = User.all
+    headers = {
+        exp: Time.now.to_i + 60 #expire in 360 seconds
+      }
+
+    unless @users.where(user_id: my_hash['user_id'])
+      @user = User.new(my_hash)
+      @user.save
+      @token = JWT.encode({user_id: my_hash['user_id']},
+                           settings.signing_key,
+                           "RS256",
+                           headers)
+      session['access_token'] = @token
+    else
+      @token = JWT.encode({user_id: my_hash['user_id']},
+                           settings.signing_key,
+                           "RS256",
+                           headers)
+      session['access_token'] = @token
+    end
+    redirect('/')
+  end
+  erb :login
+end
+
+get('/index') do
   # puts @profile[response['id']] #=> {"response"=>[{"id"=>12345, "first_name"=>"Виталий", "last_name"=>"Михайлов"}]}
   erb :index
 end
