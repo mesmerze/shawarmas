9d06d53406fd114dd507308f0cfd467039ad7de1
mesmerze <aichernovmail@gmail.com>
2017-07-13 21:30:43 +0300

    Rafactor page redirecting again!

--- app.rb
+++ app.rb
@@ -1,19 +1,18 @@
 require 'sinatra'
 require 'sinatra/activerecord'
-# require './config/environments'
 require './models/user'
 require 'yaml'
 require 'ostruct'
 require 'net/http'
 require 'vk'
 require './jwt.rb'
+
 get('/') do
   protected!
-  Vk.app_id = 6109521
-  Vk.app_secret = 'MsuqFHPDMEY0jpE2Jeyy'
+  Vk.app_id = ENV['APP_ID']
+  Vk.app_secret = ENV['APP_SECRET']
   vk = Vk.client
   @profile = vk.request 'users.get', user_ids: @user_id
-  puts @profile.each { |user| user['first_name'] }.first['first_name']
   erb :index
 end
 
@@ -24,39 +23,24 @@ get('/login') do
     shmarams = { client_id: ENV['APP_ID'],
                  client_secret: 'MsuqFHPDMEY0jpE2Jeyy',
                  redirect_uri: 'https://shawarmas.herokuapp.com/login',
-                 code: code
-    }
+                 code: code }
     uri.query = URI.encode_www_form(shmarams)
     http = Net::HTTP.get_response(uri)
     my_hash = JSON.parse(http.body)
     @users = User.all
-    headers = {
-        exp: Time.now.to_i + 60 #expire in 360 seconds
-      }
-
+    headers = { exp: Time.now.to_i + 60 } # expire in 60 seconds
     unless @users.where(user_id: my_hash['user_id'])
       @user = User.new(my_hash)
       @user.save
-      @token = JWT.encode({user_id: my_hash['user_id']},
-                           settings.signing_key,
-                           "RS256",
-                           headers)
-      session['access_token'] = @token
-    else
-      @token = JWT.encode({user_id: my_hash['user_id']},
-                           settings.signing_key,
-                           "RS256",
-                           headers)
-      session['access_token'] = @token
     end
+    @token = JWT.encode({ user_id: my_hash['user_id'] },
+                        settings.signing_key,
+                        'RS256',
+                        headers)
+    session['access_token'] = @token
     redirect('/')
   end
   erb :login
-end
-
-get('/index') do
-  # puts @profile[response['id']] #=> {"response"=>[{"id"=>12345, "first_name"=>"Виталий", "last_name"=>"Михайлов"}]}
-  erb :index
 end
 
 post('/submit') do
